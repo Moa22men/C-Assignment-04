6e819f2523f5ef704192ff7758e82cb3a9a8de6b
Moa22men <moamenabdelrahman22@gmail.com>
2025-06-30 03:48:27 +0300

    Q1 solved

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -4,9 +4,28 @@ namespace ConsoleApp5
 {
     internal class Program
     {
+        static void Value(int a)
+        {
+            a = 5;
+        }
+
+        static void Ref(ref int a)
+        {
+            a = 5;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region Q1
+            // By Value: Changes inside the function doesn't effect on the original variable.
+            // By Reference: Changes inside the function effect on the original variable.
+            int b = 10;
+
+            Value(b);
+            Console.WriteLine("Change By Value: " + b); // the output will be 10
+
+            Ref(ref b);
+            Console.WriteLine("Change By Ref: " + b); // the output will be 5
+            #endregion
         }
     }
 }
